9f7c5a9f6b93567d2b5af5af0e2c38136c4373f5
David <info@dlg.pw>
2015-12-09 15:16:44 -0500

    First commit. Adding the script

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # locate-sublime-projects-cli
 This small autocompletion plugin for ZSH allows to search for Sublime Text projects and opens them.
+
+This is only intended to work on OSX right now because it uses mdfind to quiclky search for files without creating a cache. If someone wants to make a pull request using the ZSH functions and alternatives for other platforms, you'd be welcome.
+
+## Install
+You can either add this as a zsh plugin in a OhMyZsh folder that's already included in your fpath variable or simply open
+
+```vi ~/.zshrc```
+
+And source it ad the end of the file
+
+```source ~/path/to/locate-sublime-projects/sublime-completion.zsh```
+
+## Usage
+The interest of creating such functions is to be able to move quickly in the command line from one Sublime Project to another without needing to CD.
+
+### sp $name
+Searches for a sublime project named $name and opens it
+
+### spo $name
+Searches for a Sublime project named $name and opens the parent directory of the .sublime-project file.
+
+### spcd $name
+Searches for a Sublime project named $name and cd's to the .sublime-project's directory.
+
+### Autocompletion
+mdfind (Spotlight index) is also used to provide ZSH autocompletion. Use sp + tab to see a list of all the .sublime-project files found on your computer. Finish typing the suggested basename to use the function.
---
+++ sublime-completion.zsh
@@ -0,0 +1,118 @@
+# sp searches for projects
+# opens them if one
+# lists them if many
+sp() {
+	local OLD_IFS="$IFS"
+	IFS=$'\n'
+
+	# The space between the name of the file here is only
+	# to be as fuzzy as spotlight (mdfind) cam
+	files=($(mdfind -name "$1 .sublime-project"))
+
+	if [ ${#files} -eq 0 ]; then
+		echo $fg[red] "No project found";
+	else
+		if [ ${#files} -eq 1 ]; then
+			subl "${files[1]}" --project
+		else
+			echo "$fg[red] ${#files} projects found";
+			for i in "${files[@]}"
+			do
+				echo $fg[blue] $(basename $i)
+				echo $fg[white] $i
+			done
+		fi
+	fi
+
+	IFS="$OLD_IFS"
+	return 0
+}
+
+# same as above but opens the parent directory
+spo(){
+	local OLD_IFS="$IFS"
+	IFS=$'\n'
+
+	# The space between the name of the file here is only
+	# to be as fuzzy as spotlight (mdfind) cam
+	files=($(mdfind -name "$1 .sublime-project"))
+
+	if [ ${#files} -eq 0 ]; then
+		echo $fg[red] "No project found";
+	else
+		if [ ${#files} -eq 1 ]; then
+			open $(dirname ${files[1]})
+		else
+			echo "$fg[red] ${#files} projects found";
+			for i in "${files[@]}"
+			do
+				echo $fg[blue] $(basename $i)
+				echo $fg[white] $i
+			done
+		fi
+	fi
+	
+	IFS="$OLD_IFS"
+	return 0	
+}
+
+# same as above but cd to the parent directory
+spcd(){
+	local OLD_IFS="$IFS"
+	IFS=$'\n'
+	
+	# The space between the name of the file here is only
+	# to be as fuzzy as spotlight (mdfind) cam
+	files=($(mdfind -name "$1 .sublime-project"))
+
+	if [ ${#files} -eq 0 ]; then
+		echo $fg[red] "No project found";
+	else
+		if [ ${#files} -eq 1 ]; then
+			cd $(dirname ${files[1]})
+		else
+			echo "$fg[red] ${#files} projects found";
+			for i in "${files[@]}"
+			do
+				echo $fg[blue] $(basename $i)
+				echo $fg[white] $i
+			done
+		fi
+	fi
+	
+	IFS="$OLD_IFS"
+	return 0	
+}
+
+# zsh completion for the `sp` and `spo` command
+_subl_complete() {
+	local OLD_IFS="$IFS"
+
+	IFS=$'\n'
+
+	# @todo : implement cache
+	# if _cache_invalid sp_completion; then
+		files=( $(mdfind -name "$words[2] .sublime-project") )
+		# _store_cache sp_completion files
+	# else
+		# files=_retrieve_cache
+	# fi
+	
+
+	if [ "${#files}" -eq 0 ]; then
+		echo "No project found";
+	else
+		if [ "${#files}" -eq 1 ]; then
+			compadd $(basename "$files[1]" .sublime-project)
+		else
+			for i in "${files[@]}"
+			do
+				compadd $(basename "$i" .sublime-project)
+			done
+		fi
+	fi
+
+	IFS="$OLD_IFS"
+	return 0
+}
+compdef "_subl_complete $@" sp spo spcd
